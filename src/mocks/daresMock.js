fe69aba74a54ab0bbd889a769749562362376ef4
philos <philos@Mini-von-philos.fritz.box>
2021-03-17 10:30:46 +0100

    lsj

--- src/mocks/daresMock.js
+++ src/mocks/daresMock.js
@@ -1,19 +1,21 @@
 // TODO remove eslint disable
 /* eslint-disable */
 // prettier-ignore
-[
-    {
-      "id": "1",
-      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-      "dareUser": "MacDee",
-      "daredUsers": ["userId3435", "userId3435"],
-      "category": "Private",
-      "isGlobal": "true",
-      "group": "group XY",
-      "acceptedMedia": ["image", "video", "audio"],
-      "succeeded": ["userId3435", "userId3435", "userId3435"],
-      "failed": ["userId3435", "userId3435", "userId3435"],
-      "impressions": ["fileURL2.jpg","fileURL3.jpg","fileURL4.jpg"],
-    }   
-]
+export const dareMockResponse = {
+"data":[
+        {
+          "id": 1,
+          "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+          "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+          "dareUserName": "MacDee",
+          "daredUsers": [3435, 3435],
+          "category": "Private",
+          "isGlobal": true,
+          "group": "group XY",
+          "acceptedMedia": ["image", "video", "audio"],
+          "succeeded": [3435, 3435],
+          "failed": [3435, 3435],
+          "impressions": ["fileURL2.jpg","fileURL3.jpg","fileURL4.jpg"],
+        }   
+    ]
+}
